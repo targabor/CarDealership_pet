4a10874fca2849c8e9a71c157c6730ea5ae9eb82
targabor <tar.gabor14@gmail.com>
2020-06-17 16:50:28 +0200

    I updated the start screen

---
+++ src/main/com/rofa/cardealership/MainScreen.java
@@ -0,0 +1,11 @@
+package com.rofa.cardealership;
+
+public class MainScreen {
+
+    public static void start(){
+
+        System.out.println("\t\t\tCar DealerShip");
+        System.out.println("\nChoose what you want to do!");
+
+    }
+}
